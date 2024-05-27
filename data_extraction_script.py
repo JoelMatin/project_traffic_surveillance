0696e6e66e85546fc33e255ce0e2478873cb4466
PachaAfghan <joel.matin2@gmail.com>
2024-05-27 02:31:26 +0200

    neural network file

--- data_extraction_script.py
+++ data_extraction_script.py
@@ -2,17 +2,19 @@ import pandas as pd
 import numpy as np
 
 
-data = pd.read_csv('US_City_Temp_Data.csv')
-l = [1, 1, 1]
-nb_rows, nb_columns = data.shape
-T = np.zeros((nb_columns-1, nb_rows))
-t = np.array(data.iloc[:, 0])
-cities = np.array(data.columns[1:])
-i = 0
-for city in cities:
-     T[i] = np.array(data[city])
-     i += 1
-print(cities)
-print(T)
-print(t)
+# data = pd.read_csv('US_City_Temp_Data.csv')
+# nb_rows, nb_columns = data.shape
+# T = np.zeros((nb_columns-1, nb_rows))           ## Array where each element is an array with the temperatures of a city over the years
+# t = np.array(data.iloc[:, 0])                   ## Array with the dates of temperature measurement
+# cities = np.array(data.columns[1:])             ## Array with the names of the cities
+# i = 0
+# for city in cities:
+#      T[i] = np.array(data[city])
+#      i += 1
+# print(cities)
+# print(T)
+# print(t)
+
+l = np.zeros(8)
+print(l[8])
 
